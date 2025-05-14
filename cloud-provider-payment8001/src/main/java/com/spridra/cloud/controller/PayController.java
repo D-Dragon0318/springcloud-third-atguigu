d6fc794c3444bf3d530afae4d3192913241224e9
Spridra <2394527308@qq.com>
2025-05-14 16:33:16 +0800

    feat: 统一返回类修改controller

--- cloud-provider-payment8001/src/main/java/com/spridra/cloud/controller/PayController.java
+++ cloud-provider-payment8001/src/main/java/com/spridra/cloud/controller/PayController.java
@@ -2,6 +2,7 @@ package com.spridra.cloud.controller;
 
 import com.spridra.cloud.entities.Pay;
 import com.spridra.cloud.entities.PayDTO;
+import com.spridra.cloud.resp.ResultData;
 import com.spridra.cloud.service.IPayService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -28,42 +29,44 @@ public class PayController
 
     @PostMapping(value = "/pay/add")
     @Operation(summary = "新增",description = "新增支付流水方法,json串做参数")
-    public String addPay(@RequestBody Pay pay)
+    public ResultData<String> addPay(@RequestBody Pay pay)
     {
         System.out.println(pay.toString());
         int i = payService.add(pay);
-        return "成功插入记录，返回值："+i;
+        return ResultData.success("成功插入记录，返回值："+i);
     }
 
     @DeleteMapping(value = "/pay/del/{id}")
     @Operation(summary = "删除",description = "删除支付流水方法")
-    public Integer deletePay(@PathVariable("id") Integer id) {
-        return payService.delete(id);
+    public ResultData<Integer> deletePay(@PathVariable("id") Integer id) {
+        int i = payService.delete(id);
+        return ResultData.success(i);
     }
 
     @PutMapping(value = "/pay/update")
     @Operation(summary = "修改",description = "修改支付流水方法")
-    public String updatePay(@RequestBody PayDTO payDTO)
+    public ResultData<String> updatePay(@RequestBody PayDTO payDTO)
     {
         Pay pay = new Pay();
         BeanUtils.copyProperties(payDTO, pay);
 
         int i = payService.update(pay);
-        return "成功修改记录，返回值："+i;
+        return ResultData.success("成功修改记录，返回值："+i);
     }
 
     @GetMapping(value = "/pay/get/{id}")
     @Operation(summary = "按照ID查流水",description = "查询支付流水方法")
-    public Pay getById(@PathVariable("id") Integer id)
+    public ResultData<Pay> getById(@PathVariable("id") Integer id)
     {
-        return payService.getById(id);
+        Pay pay = payService.getById(id);
+        return ResultData.success(pay);
     }
 
     @GetMapping(value = "/pay/getAll")
-    public List<Pay> getAll(){
+    public ResultData<List<Pay>> getAll(){
         log.info("查询所有记录");
         List<Pay> payList = payService.getAll();
         log.info("查询结果：{}", payList.toString());
-        return payList;
+        return ResultData.success(payList);
     }
 }
