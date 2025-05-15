e767277fae7afb0970e9dfb2e640995e68e2fce2
Spridra <2394527308@qq.com>
2025-05-15 17:24:10 +0800

    feat: 测试openfeign远程调用负载均衡

---
+++ cloud-api-commons/src/main/java/com/spridra/cloud/apis/PayFeignApi.java
@@ -0,0 +1,42 @@
+package com.spridra.cloud.apis;
+
+import com.spridra.cloud.entities.PayDTO;
+import com.spridra.cloud.resp.ResultData;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+/**
+ * @Author: Spridra
+ * @CreateTime: 2025-05-15 16:58
+ * @Describe: 订单远程调用支付模块
+ * @Version: 1.0
+ */
+
+@FeignClient(value = "cloud-payment-service")
+public interface  PayFeignApi {
+        /**
+         * 新增一条支付相关流水记录
+         * @param payDTO
+         * @return
+         */
+        @PostMapping("/pay/add")
+        public ResultData addPay(@RequestBody PayDTO payDTO);
+
+        /**
+         * 按照主键记录查询支付流水信息
+         * @param id
+         * @return
+         */
+        @GetMapping("/pay/get/{id}")
+        public ResultData getPayInfo(@PathVariable("id") Integer id);
+
+        /**
+         * openfeign天然支持负载均衡演示
+         * @return
+         */
+        @GetMapping(value = "/pay/get/info")
+        public String mylb();
+    }
--- cloud-consumer-feign-order90/src/main/java/com/spridra/cloud/Main.java
+++
@@ -1,7 +0,0 @@
-package com.spridra.cloud;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
