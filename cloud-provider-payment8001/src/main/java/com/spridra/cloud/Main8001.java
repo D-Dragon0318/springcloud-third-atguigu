48a5920adf71187ab8e1c3bb8eb08282c6c3f06a
Spridra <2394527308@qq.com>
2025-05-15 16:09:41 +0800

    feat: 服务配置consul自动刷新

--- cloud-provider-payment8001/src/main/java/com/spridra/cloud/Main8001.java
+++ cloud-provider-payment8001/src/main/java/com/spridra/cloud/Main8001.java
@@ -3,6 +3,7 @@ package com.spridra.cloud;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import tk.mybatis.spring.annotation.MapperScan;
 
 /**
@@ -14,6 +15,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 @SpringBootApplication
 @MapperScan("com.spridra.cloud.mapper")
 @EnableDiscoveryClient
+@RefreshScope
 public class Main8001 {
     public static void main(String[] args) {
         SpringApplication.run(Main8001.class, args);
