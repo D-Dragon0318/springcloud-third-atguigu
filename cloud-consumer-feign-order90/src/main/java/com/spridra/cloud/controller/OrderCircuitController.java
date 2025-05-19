96789c4cc1cc109f7dcb49f04193deee1932646d
Spridra <2394527308@qq.com>
2025-05-19 15:14:50 +0800

    feat: 固定线程池舱壁
    FixedThreadPoolBulkhead的功能与SemaphoreBulkhead一样也是用于限制并发执行的次数的，但是二者的实现原理存在差别而且表现效果也存在细微的差别。
    FixedThreadPoolBulkhead使用一个固定线程池和一个等待队列来实现舱壁。
    当线程池中存在空闲时，则此时进入系统的请求将直接进入线程池开启新线程或使用空闲线程来处理请求。
    当线程池中无空闲时时，接下来的请求将进入等待队列，
       若等待队列仍然无剩余空间时接下来的请求将直接被拒绝，
       在队列中的请求等待线程池出现空闲时，将进入线程池进行业务处理。

--- cloud-consumer-feign-order90/src/main/java/com/spridra/cloud/controller/OrderCircuitController.java
+++ cloud-consumer-feign-order90/src/main/java/com/spridra/cloud/controller/OrderCircuitController.java
@@ -8,6 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Author: Spridra
  * @CreateTime: 2025-05-16 15:23
@@ -47,5 +50,25 @@ public class OrderCircuitController
     public String myBulkheadFallback(Throwable t)
     {
         return "myBulkheadFallback，隔板超出最大数量限制，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~";
+    }
+
+    /**
+     * (船的)舱壁,隔离,THREADPOOL
+     * @param id
+     * @return
+     */
+    @GetMapping(value = "/feign/pay/circuit/testThreadPoolBulkhead/{id}")
+    @Bulkhead(name = "cloud-payment-service",fallbackMethod = "myBulkheadPoolFallback",type = Bulkhead.Type.THREADPOOL)
+    public CompletableFuture<String> myBulkheadTHREADPOOL(@PathVariable("id") Integer id)
+    {
+        System.out.println(Thread.currentThread().getName()+"\t"+"enter the method!!!");
+        try { TimeUnit.SECONDS.sleep(3); } catch (InterruptedException e) { e.printStackTrace(); }
+        System.out.println(Thread.currentThread().getName()+"\t"+"exist the method!!!");
+
+        return CompletableFuture.supplyAsync(() -> payFeignApi.testMyBulkhead(id) + "\t" + " Bulkhead.Type.THREADPOOL");
+    }
+    public CompletableFuture<String> myBulkheadPoolFallback(Integer id,Throwable t)
+    {
+        return CompletableFuture.supplyAsync(() -> "Bulkhead.Type.THREADPOOL，系统繁忙，请稍后再试-----/(ㄒoㄒ)/~~");
     }
 }
