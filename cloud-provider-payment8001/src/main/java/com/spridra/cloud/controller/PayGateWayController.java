e29b0db77a7aa3e67ce0a1a5fe066a7948c6ce82
Spridra <2394527308@qq.com>
2025-05-20 15:13:47 +0800

    feat: Gateway filter 添加请求头内容

--- cloud-provider-payment8001/src/main/java/com/spridra/cloud/controller/PayGateWayController.java
+++ cloud-provider-payment8001/src/main/java/com/spridra/cloud/controller/PayGateWayController.java
@@ -1,5 +1,6 @@
 package com.spridra.cloud.controller;
 
+import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.IdUtil;
 import com.spridra.cloud.entities.Pay;
 import com.spridra.cloud.resp.ResultData;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Enumeration;
 import java.util.Map;
 
 /**
@@ -54,5 +56,23 @@ public class PayGateWayController
     public ResultData<String> getGatewayInfo()
     {
         return ResultData.success("gateway info test："+ IdUtil.simpleUUID());
+    }
+
+    @GetMapping(value = "/pay/gateway/filter")
+    public ResultData<String> getGatewayFilter(HttpServletRequest request)
+    {
+        String result = "";
+        Enumeration<String> headers = request.getHeaderNames();
+        while(headers.hasMoreElements())
+        {
+            String headName = headers.nextElement();
+            String headValue = request.getHeader(headName);
+            System.out.println("请求头名: " + headName +"\t\t\t"+"请求头值: " + headValue);
+            if(headName.equalsIgnoreCase("X-Request-atguigu1")
+                    || headName.equalsIgnoreCase("X-Request-atguigu2")) {
+                result = result+headName + "\t " + headValue +" ";
+            }
+        }
+        return ResultData.success("getGatewayFilter 过滤器 test： "+result+" \t "+ DateUtil.now());
     }
 }
